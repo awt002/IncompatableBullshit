479ab018a8c5c74c5f44b0c46600b22332da56f7
Aaron W Taylor <awt002@UCSD.EDU>
2016-04-19 17:19:51 -0700

    fuk this

---
+++ app/src/androidTest/java/tests/JUnit_tests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.awt002.oldincompatiblebullshitpart1.MainActivity;
+import com.example.awt002.oldincompatiblebullshitpart1.R;
+
+/**
+ * Created by Aaron on 4/19/2016.
+ */
+public class JUnit_tests extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    MainActivity mainActivity;
+    public JUnit_tests()
+    {
+        super(MainActivity.class);
+    }
+
+    public void test_first()
+    {
+        mainActivity = getActivity();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Hello World", tester);
+    }
+}
